1bd33773d90ea7009685c0f8c67ca25fdd7f311c
gezy369 <107927231+gezy369@users.noreply.github.com>
2022-08-16 14:43:46 +0200

    Update GoogleChart.php

--- charts/GoogleChart.php
+++ charts/GoogleChart.php
@@ -75,5 +75,43 @@
       var chart = new google.visualization.PieChart(document.getElementById('longshortchart'));
       chart.draw(data, options);
     }
+        
+        
+    /*---------------------------------------------------------------------------
+      P&L top line chart
+    ---------------------------------------------------------------------------*/
+    google.charts.setOnLoadCallback(drawPLtopChart);
+
+    function drawPLtopChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Months',    'Balance', 'Threshold'],
+        ['January',   50000,      47500],
+        ['February',  52000,      47700],
+        ['March',     51000,      47700],
+        ['April',     55000,      48000],
+        ['May',       58000,      48000],
+        ['June',      59500,      48000],
+        ['July',      57850,      48000],
+        ['August',    58000,      48000],
+        ['September', 60000,      48000],
+        ['October',   59800,      48000],
+        ['November',  62000,      48000],
+        ['December',  62350,      48000],
+      ]);
+
+      var options = {
+        title: 'Account Balance',
+        curveType: 'function',
+        legend: { position: 'top' },
+        //animation: {startup: 'true', duration: 1000},
+        crosshair: { trigger: 'both', orientation: 'vertical', color: 'grey' },
+        tooltip: {trigger: 'both'}
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('pltopchart'));
+
+      chart.draw(data, options);
+    }
     </script>
+    
 </html>
